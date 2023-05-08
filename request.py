5805ba6b39057aec00f4315251fef4675c94cc39
Gabriel Colli <gabrielcolli@microsoft.com>
2023-05-08 16:03:52 -0500

    second commit

--- request.py
+++ request.py
@@ -4,10 +4,10 @@ import requests
 body = {
   "Name": {
     "FirstName": "Gabriel",
-    "LastName": "Colli"
+    "LastName": "myname"
     }
   }
 
 
-response = requests.post("https://gabrielcolliapi.azurewebsites.net/api/HttpTrigger1", json=body)
+response = requests.post("your function app here", json=body)
 print(response.text,response)
